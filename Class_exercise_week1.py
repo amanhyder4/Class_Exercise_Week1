16b0a158cc3800627f50ba1c11bf3ffe69cc4675
akhily21 <104872907+akhily21@users.noreply.github.com>
2022-05-03 19:47:10 -0400

    Update Class_exercise_week1.py

--- Class_exercise_week1.py
+++ Class_exercise_week1.py
@@ -2,7 +2,12 @@ a = input('Enter first value')
 b = input('Enter second value')
 a= int(a)
 b= int(b)
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
+
+c=10
+print((a+b)+c)
+print((a-b)+c)
+print((a*b)+c)
+print((a/b)+c)
+
+
+
